a8dd5d3bf30cfab1858c9a7d27cb5e5402305fa8
ToffeeFox <luki@toffeefox.com>
2026-08-24 19:42:15 -0500

    Force remove /opt/cloudflare-warp/ before trying to install the Warp Agent

--- build_files/build.sh
+++ build_files/build.sh
@@ -16,6 +16,9 @@ cp -avf "/ctx/system_files"/. /
 dnf5 install -y tmux
 
 # Install Cloudflare Warp
+## Clean the target directory first
+rm -rf /opt/cloudflare-warp/
+## NOW install CF Warp
 dnf5 install -y cloudflare-warp
 
 # Use a COPR Example:
