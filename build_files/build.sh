4ee581a777509158a173a04f45425333e6e1be57
ToffeeFox <luki@toffeefox.com>
2026-08-24 21:00:03 -0500

    Remove redundant tmux install command

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,9 +12,6 @@ cp -avf "/ctx/system_files"/. /
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux
-
 # Install Cloudflare Warp
 dnf5 install -y cloudflare-warp
 
